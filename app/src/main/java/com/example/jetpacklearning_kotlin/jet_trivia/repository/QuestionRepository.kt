8e18af7525971ea313075802863111ad02370b5c
majid2851 <majidbagheri2851@gmail.com>
2022-12-12 15:32:53 +0330

    oreilly part=>77 is Completed.

--- app/src/main/java/com/example/jetpacklearning_kotlin/jet_trivia/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/jetpacklearning_kotlin/jet_trivia/repository/QuestionRepository.kt
@@ -1,16 +1,30 @@
 package com.example.jetpacklearning_kotlin.jet_trivia.repository
 
+import android.util.Log
 import com.example.jetpacklearning_kotlin.jet_trivia.data.DataOrException
-import com.example.jetpacklearning_kotlin.jet_trivia.model.QuestionModel
+import com.example.jetpacklearning_kotlin.jet_trivia.model.QuestionModelItem
 import com.example.jetpacklearning_kotlin.jet_trivia.network.QuestionApi
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api:QuestionApi)
 {
-    private val questionList=
-        DataOrException<ArrayList<QuestionModel>,Boolean,Exception>()
+    private val dataOrException=
+        DataOrException<ArrayList<QuestionModelItem>,Boolean,Exception>()
 
+    suspend fun getAllQuestions():DataOrException<ArrayList<QuestionModelItem>,Boolean,Exception>
+    {
+        try
+        {
+            dataOrException.loading=true
+            dataOrException.data=api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()){ dataOrException.loading=false }
 
+        }catch (exception:Exception){
+            dataOrException.e=exception
+            Log.d("Exc","getAllQuestions:"+exception.toString())
+        }
+        return dataOrException
+    }
 
 
 
