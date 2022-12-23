2273c27785905a8e4e57a437be0c4ec2b68336f5
majid2851 <majidbagheri2851@gmail.com>
2022-12-23 17:47:27 +0330

    oreilly part=>85 is Completed.

--- app/src/main/java/com/example/jetpacklearning_kotlin/jet_trivia/component/Questions.kt
+++ app/src/main/java/com/example/jetpacklearning_kotlin/jet_trivia/component/Questions.kt
@@ -30,22 +30,32 @@ import androidx.compose.ui.unit.sp
 import com.example.jetpacklearning_kotlin.jet_trivia.model.QuestionModelItem
 import com.example.jetpacklearning_kotlin.jet_trivia.screens.QuestionsViewModel
 import com.example.jetpacklearning_kotlin.jet_trivia.util.AppColors
+import java.lang.Exception
 
 @Composable
 fun Questions(viewModel: QuestionsViewModel)
 {
     val questions=viewModel.getMyData().value.data?.toMutableList()
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
     if(viewModel.getMyData().value.loading==true)
     {
         CircularProgressIndicator()
         Log.i("mag2851-loading==>", "true")
     }else
     {
+        val question=try {
+            questions?.get(questionIndex.value)
+        }catch (e:Exception){
+            null
+        }
         if (questions!=null)
         {
-            QuestionDisplay(question = questions.first())
+            QuestionDisplay(question = question!!, questionIndex = questionIndex,
+            viewModel = viewModel)
             {
-
+                questionIndex.value=questionIndex.value+1
             }
         }
     }
@@ -56,8 +66,8 @@ fun Questions(viewModel: QuestionsViewModel)
 @Composable
 fun QuestionDisplay(
     question:QuestionModelItem,
-//    questionIndex:MutableState<Int>,
-//    viewModel: QuestionsViewModel,
+    questionIndex:MutableState<Int>,
+    viewModel: QuestionsViewModel,
     onNextClick:(Int)->Unit={}
     )
 {
@@ -82,16 +92,17 @@ fun QuestionDisplay(
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()
-        .padding(4.dp),
-        color = AppColors.mDarkPurple)
+//        .padding(4.dp)
+        ,color = AppColors.mDarkPurple)
     {
         Column(modifier=Modifier.padding(12.dp),verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start)
         {
-            QuestionTracker()
+            QuestionTracker(questionIndex.value)
             DrawDottedLine(pathEffect =pathEffect )
             QuestionTitle(question)
-            choicesState.forEachIndexed { index, answer ->
+            choicesState.forEachIndexed()
+            { index, answer ->
                 Row(modifier= Modifier
                     .padding(4.dp)
                     .fillMaxWidth()
@@ -127,13 +138,47 @@ fun QuestionDisplay(
                             Color.Red.copy(alpha = .2f)
                         }
                     ))
-                    Text(text = answer)
+                    val annotatedString= buildAnnotatedString()
+                    {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Light,
+                            color = if (correctAnswerState.value==true &&
+                                index==answerState.value)
+                            {
+                                Color.Green
+                            }else if (correctAnswerState.value==false
+                                && index==answerState.value)
+                            {
+                                Color.Red
+                            }else{AppColors.mOffWhite}, fontSize = 17.sp
+                        ))
+                        {
+                            append(answer )
+                        }
+                    }
+                    Text(text = annotatedString, modifier = Modifier.padding(6.dp))
+
                     
                 }
             }
 
+            Button(onClick = {onNextClick(questionIndex.value)},
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(alignment = Alignment.CenterHorizontally)
+                        , shape = RoundedCornerShape(35.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = AppColors.mLightBlue
+                        ))
+                    {
+                        Text(text = "Next", modifier = Modifier.padding(6.dp),
+                            fontSize = 17.sp, fontWeight = FontWeight.Bold,
+                            color=AppColors.mOffWhite)
+                    }
+
 
         }
+
+
     }
 }
 
