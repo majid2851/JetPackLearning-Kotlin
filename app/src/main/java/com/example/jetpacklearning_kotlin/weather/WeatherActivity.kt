7b5d37b7ebe4f2cb4d0a74f4568f279040434472
majid2851 <majidbagheri2851@gmail.com>
2022-12-25 10:54:32 +0330

    oreilly part=>95 is Completed.

--- app/src/main/java/com/example/jetpacklearning_kotlin/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/jetpacklearning_kotlin/weather/WeatherActivity.kt
@@ -3,13 +3,18 @@ package com.example.jetpacklearning_kotlin.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpacklearning_kotlin.weather.navigation.WeatherNavigation
 import com.example.jetpacklearning_kotlin.weather.ui.theme.JetPackLearningKotlinTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,15 +27,26 @@ class WeatherActivity : ComponentActivity()
         super.onCreate(savedInstanceState)
         setContent()
         {
-            JetPackLearningKotlinTheme()
+            WeatherApp()
+        }
+    }
+}
+
+@Composable
+fun WeatherApp()
+{
+    JetPackLearningKotlinTheme()
+    {
+        Surface(modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background)
+        {
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally)
             {
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background)
-                {
+                WeatherNavigation()
 
-
-                }
             }
+
         }
     }
 }
